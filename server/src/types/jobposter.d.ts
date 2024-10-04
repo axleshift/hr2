13a8bb6278601c9d097c6eff1cc616c2f4515655
freight-capstone <>
2024-10-04 07:04:19 +0000

    Build server

--- server/src/types/jobposter.d.ts
+++
@@ -1,9 +0,0 @@
-export interface JobposterType {
-    _id: string;
-    platform: string;
-    isApproved: boolean;
-    isPosted: boolean;
-    content: string;
-    post_id: string;
-    ref_id: string;
-}
