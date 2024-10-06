30f5cab214c192085ce1b3e831ae031f3ae79a18
SabishiCake <pedjovensindol.edu@gmail.com>
2024-10-06 22:03:20 +0800

    Refactor auth routes to use login and logout functions

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,9 +4,10 @@ const router = Router();
 import dotenv from "dotenv";
 dotenv.config();
 
-import { createUser, verifyUser } from "../database/controllers/authController";
+import { createUser, login, logout } from "../database/controllers/authController";
 
 router.post("/register", createUser);
-router.post("/login", verifyUser);
+router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
--- server/src/routes/jobposter.ts
+++ server/src/routes/jobposter.ts
@@ -4,10 +4,10 @@ const router = Router();
 import dotenv from "dotenv";
 dotenv.config();
 
-import { createJobposter, getJobposterById, removeJobposter } from "../database/controllers/jobposterController";
+import { createJobposter, getJobposterByRefId, removeJobposter } from "../database/controllers/jobposterController";
 
 router.post("/:id/post", createJobposter);
-router.get("/:id", getJobposterById);
-router.post("/:id/delete", removeJobposter);
+router.get("/:id", getJobposterByRefId);
+router.delete("/:id", removeJobposter);
 
 export default router;
