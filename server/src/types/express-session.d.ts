da98ced4187735e8c50f485cc207e9f7bba48bdd
SabishiCake <pedjovensindol.edu@gmail.com>
2024-10-06 22:01:47 +0800

    Refactor sessionUser interface and add token property

--- server/src/types/express-session.d.ts
+++ server/src/types/express-session.d.ts
@@ -1,10 +1,11 @@
 import "express-session";
 
-interface SessionUser {
+export interface SessionUser {
     username: string;
     role: string;
     email: string;
     status: string;
+    token: string;
 }
 
 declare module "express-session" {
--- server/src/types/tweet.d.ts
+++ server/src/types/tweet.d.ts
@@ -1,3 +1,10 @@
 export interface TweetType {
     id_str: string;
 }
+
+export interface ITweet {
+    data: {
+        id: string;
+    };
+    errors?: unknown;
+}
