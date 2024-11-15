c1214b9029f77886c0d9d37decbb185ca35f5b03
Pedjoven Sindol <127638635+Podjisin@users.noreply.github.com>
2024-11-15 19:25:10 +0800

    style: Format code for consistency and readability in AppFooter and config files

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { CFooter } from '@coreui/react'
 
 const AppFooter = () => {
-    return (
-        <CFooter className="px-4">
-            <div>Axleshift HR 2&nbsp;&copy;&nbsp;2024</div>
-            <div className="ms-auto"></div>
-        </CFooter>
-    )
+  return (
+    <CFooter className="px-4">
+      <div>Axleshift HR 2&nbsp;&copy;&nbsp;2024</div>
+      <div className="ms-auto"></div>
+    </CFooter>
+  )
 }
 
 export default React.memo(AppFooter)
--- client/src/config.js
+++ client/src/config.js
@@ -1,11 +1,11 @@
 export const config = {
-    server: {
-        url: import.meta.env.VITE_REACT_SERVER_URL
+  server: {
+    url: import.meta.env.VITE_REACT_SERVER_URL,
+  },
+  google: {
+    recaptcha: {
+      siteKey: import.meta.env.VITE_REACT_GOOGLE_RECAPTCHA_SITE_KEY,
+      secretKey: import.meta.env.VITE_REACT_GOOGLE_RECAPTCHA_SECRET_KEY,
     },
-    google: {
-        recaptcha: {
-            siteKey: import.meta.env.VITE_REACT_GOOGLE_RECAPTCHA_SITE_KEY,
-            secretKey: import.meta.env.VITE_REACT_GOOGLE_RECAPTCHA_SECRET_KEY
-        },
-    }
+  },
 }
