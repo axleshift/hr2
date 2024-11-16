f1937019c61cdb0855429a62adee5286f94fe76c
freight-capstone <pedjovensindol.edu@gmail.com>
2024-11-16 07:15:23 +0000

    Build server

--- dist/index.js
+++ dist/index.js
@@ -55,6 +55,7 @@ const initializeFolders = () => {
 };
 initializeFolders();
 // Middlewares
+app.set("trust proxy", true);
 app.use((0, cors_1.default)({
     // multiple origins can be added
     origin: config_1.config.server.origins,
