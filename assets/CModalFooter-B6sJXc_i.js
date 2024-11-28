185d503164f8aab8f54379820f8d8958e94cb8de
freight-capstone <pedjovensindol.edu@gmail.com>
2024-11-28 13:19:46 +0000

    Build client

--- assets/CModalFooter-B6sJXc_i.js
+++
@@ -1,1 +0,0 @@
-import{r as t,_ as d,R as m,a as n,c as p,p as e}from"./index-DrjcOHzu.js";var s=t.forwardRef(function(a,r){var o=a.children,c=a.className,l=d(a,["children","className"]);return m.createElement("div",n({className:p("modal-footer",c)},l,{ref:r}),o)});s.propTypes={children:e.node,className:e.string};s.displayName="CModalFooter";export{s as C};
---
+++ assets/CModalFooter-wsljN6_3.js
@@ -0,0 +1,1 @@
+import{r as t,_ as d,R as m,a as n,c as p,p as e}from"./index-UTTuAW8U.js";var s=t.forwardRef(function(a,r){var o=a.children,c=a.className,l=d(a,["children","className"]);return m.createElement("div",n({className:p("modal-footer",c)},l,{ref:r}),o)});s.propTypes={children:e.node,className:e.string};s.displayName="CModalFooter";export{s as C};
