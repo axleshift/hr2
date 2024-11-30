f2cd364316c4892d98cc52450a05227955ad7e90
Pedjoven Sindol <127638635+Podjisin@users.noreply.github.com>
2024-11-30 21:51:37 +0800

    Enhance applicant views with search functionality and improved table layout; add cards and icons for better user experience

--- client/src/views/applicants/Training.js
+++ client/src/views/applicants/Training.js
@@ -1,17 +1,71 @@
-import { CContainer, CRow, CCol } from '@coreui/react'
+import {
+  CContainer,
+  CRow,
+  CCol,
+  CCard,
+  CCardBody,
+  CTable,
+  CTableHead,
+  CTableBody,
+  CTableHeaderCell,
+  CTableDataCell,
+  CTableRow,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CButton,
+} from '@coreui/react'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core'
+import { faRefresh, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Training = () => {
   return (
     <>
       <CContainer>
-        <CRow>
+        <CRow className="mb-3">
           <CCol>
             <h2>Training</h2>
             <small>
               In this page, you can view and edit the list of applicants which needs training for
               the job, or is currently undergoing training.
             </small>
+          </CCol>
+        </CRow>
+        <CRow className="mb-3">
+          <CCol>
+            <CForm>
+            <CInputGroup>
+                <CFormInput type="search" placeholder="Search..." aria-label="Search" />
+                <CButton type="button" color="primary">
+                  <FontAwesomeIcon icon={faSearch} />
+                </CButton>
+                <CButton type="button" color="primary">
+                  <FontAwesomeIcon icon={faRefresh} />
+                </CButton>
+              </CInputGroup>
+            </CForm>
+          </CCol>
+        </CRow>
+        <CRow className="mb-3">
+          <CCol>
+            <CCard>
+              <CCardBody>
+                <CTable>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell>Applicant ID</CTableHeaderCell>
+                      <CTableHeaderCell>Applicant Name</CTableHeaderCell>
+                      <CTableHeaderCell>Job Title</CTableHeaderCell>
+                      <CTableHeaderCell>Application Date</CTableHeaderCell>
+                      <CTableHeaderCell>Actions</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody></CTableBody>
+                </CTable>
+              </CCardBody>
+            </CCard>
           </CCol>
         </CRow>
       </CContainer>
