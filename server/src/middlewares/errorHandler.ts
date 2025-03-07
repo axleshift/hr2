15caa4b7e756489e4e1a14dd27277f179d4b0e7b
Podjisin <pedjovensindol.edu@gmail.com>
2025-03-07 13:19:34 +0800

    Enhance error handler to validate response object and include NextFunction parameter

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import logger from "./logger";
 
 interface CustomError extends Error {
   status?: number;
 }
 
-const errorHandler = (err: CustomError, req: Request, res: Response) => {
+const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  if (!(res instanceof Response)) {
+    return next(new Error("Invalid response object"));
+  }
+
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
 
