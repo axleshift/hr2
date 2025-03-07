0e1c07097879fecc34a42a9629f9bcf5ff020080
freight-capstone <pedjovensindol.edu@gmail.com>
2025-03-07 08:06:17 +0000

    Build server

--- dist/middlewares/errorHandler.js
+++ dist/middlewares/errorHandler.js
@@ -4,7 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = __importDefault(require("./logger"));
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
+    if (!(res instanceof Response)) {
+        return next(new Error("Invalid response object"));
+    }
     const status = err.status || 500;
     const message = err.message || "Internal Server Error";
     logger_1.default.error(`${status} - ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
