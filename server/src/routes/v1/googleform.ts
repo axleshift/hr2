32fc76fed037d13039c2a34fbe28491452b33153
Podjisin <pedjovensindol.edu@gmail.com>
2025-03-11 20:52:28 +0800

    Remove session verification middleware from Google Form submission route.

--- server/src/routes/v1/googleform.ts
+++ server/src/routes/v1/googleform.ts
@@ -3,15 +3,10 @@ const router = Router();
 
 import dotenv from "dotenv";
 dotenv.config();
-import verifySession from "../../middlewares/verifySession";
-
 import { formSubmit } from "../../database/v1/controllers/googleFormController";
 
 router.post(
   "/submit",
-  verifySession({
-    permissions: ["webhook", "admin"],
-  }),
   formSubmit
 );
 
